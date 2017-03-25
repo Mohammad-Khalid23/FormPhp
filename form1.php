632fc9e168bff0bc0a206186db1ef5dc1b3552e2
Mohammad Khalid <m.k.ayub23@gmail.com>
2017-03-25 18:35:37 +0500

    PHP form Practice

---
+++ form1.php
@@ -0,0 +1,42 @@
+<!--<?php
+
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$gender = (isset($_REQUEST['gender']) ? $_REQUEST['gender'] : '');
+$age = $_REQUEST['age'];
+$comment = $_REQUEST['comment'];
+
+
+
+echo "Name: ".$name."<br>";
+echo "Email: ".$email."<br>";
+echo "Gender: ".$gender."<br>";
+echo "Age: ".$age."<br>";
+echo "Comments: ".$comment;
+
+
+
+?>-->
+
+<form action="handle.php" method="post">
+
+
+ <p> <label for="name">Name: </label><input type="text" id="name" name="name" size="20" maxlength="40"> <p/>  
+
+ <p> <label for="email">Email: </label><input type="email" id="email" name="email" size="20" maxlength="40"> <p/>
+
+ <p> Gender:<input type="radio" name="gender" id="male" value="M"><label for="male" >Male</label>
+            <input type="radio" name="gender" id="female" value="F"><label for="female">Female</label> <br/> <p/>
+
+<p>
+<label for="age">Age:</label>
+<select name="age" id="age">
+                <option value="0-29">Under 30<option/>
+                <option value="30-60">Between 30 and 60<option/>
+                <option value="60+">Over 60<option/>
+<select/> <p/>
+
+<label for="comment">Comments: </label><textarea name="comment" id="comment" cols="40" rows="3"></textarea> <p/>
+<p>  <input type="submit" name="submit" value="Submit"> <p/>
+
+ </form>
---
+++ userForm.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<?php require_once('title.php'); ?>
+</head>
+<body>
+
+
+<?php include('form1.php'); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ <!--<form action="handle.php" method="post">
+
+
+ <p> <label for="name">Name: </label><input type="text" id="name" name="name" size="20" maxlength="40"> <p/>  
+
+ <p> <label for="email">Email: </label><input type="email" id="email" name="email" size="20" maxlength="40"> <p/>
+
+ <p> Gender:
+     <input type="radio" name="gender" id="male" value="M"><label for="male" >Male</label>
+
+ <input type="radio" name="gender" id="female" value="F"><label for="female">Female</label> <br/> <p/>
+
+<p>
+<label for="age">Age:</label><select name="age" id="age">
+                <option value="0-29">Under 30<option/>
+                <option value="30-60">Between 30 and 60<option/>
+                <option value="60+">Over 60<option/>
+                    <select/> <p/>
+
+  
+     <label for="comment">Comments: </label><textarea name="comment" id="comment" cols="40" rows="3"></textarea> 
+<p/>
+      <p>  <input type="submit" name="submit" value="Submit"> <p/>
+
+ </form>-->
+
+
+
+</body>
+</html>
